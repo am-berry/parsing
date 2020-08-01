da910b1e39f6aa01e927d0c3c9863b7a9eba512c
Aman Berry <aman.berry725@gmai.com>
2020-08-01 19:35:42 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,21 @@ extern crate serde;
 use serde_json::{Result, Value};
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
 struct Data {
-    selftext: str,
+    selftext: String,
 }
 
-fn main() -> Result<()> {
+fn read_from_file<P: AsRef<Path>>(path: P) -> Result<Data, Box<Error>> {
     let stdin = std::io::stdin();
     let stdin = stdin.lock();
 
-    let deserializer = serde_json::Deserializer::from_reader(stdin);
+    let deserializer = serde_json::Deserializer::from_reader("./data/2011-01.json");
     let iterator = deserializer.into_iter::<serde_json::Value>();
     for item in iterator {
         println!("Got {:?}", item?);

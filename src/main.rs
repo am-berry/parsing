93fc76acdef045550f0dcca7bcbc69324bcc62af
Aman Berry <aman.berry725@gmai.com>
2020-08-01 21:55:07 +0100

    got json parser working

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,19 @@
-extern crate serde;
+extern crate ajson;
 
-use serde_json::{Result, Value};
-use serde::{Deserialize};
-
-use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
-
-#[derive(Deserialize, Debug)]
-struct Data {
-    selftext: String,
-}
-
-fn read_from_file<P: AsRef<Path>>(path: P) -> Result<()> {
-    let file = File::open(path);
-    let reader = BufReader::new(file);
-    
-    let deserializer = serde_json::Deserializer::from_reader(reader);
-    let iterator = deserializer.into_iter::<Value>();
-    for item in iterator {
-        let v: Value = item["selftext"];
-    Ok(())
-}
+use std::io::{BufRead, BufReader};
 
 fn main() {
-   read_from_file("./data/2011-01.json").unwrap();
+    let mut vals = Vec::new();
+    let f = File::open("src/data/2011-01.json").unwrap();
+    let reader = BufReader::new(f);
+
+    for item in reader.lines() {
+        let txt = ajson::get(&item.unwrap(), "selftext").unwrap().as_str().to_lowercase();
+        if txt.contains("tl;dr") | txt.contains("tl:dr") {
+            vals.push(txt);
+        }
+    } 
+    println!("{:?}", vals);
 }
+

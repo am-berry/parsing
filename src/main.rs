32f3d0e8cdfcc54ecfa582d635d4e30641c108c7
Aman Berry <aman.berry725@gmai.com>
2020-08-01 19:28:51 +0100

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-extern crate serde_derive;
+extern crate serde;
 
 use serde_json::{Result, Value};
-use std::fs::File;
-use std::io::copy;
-use std::io::stdout;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    selftext: str,
+}
 
 fn main() -> Result<()> {
-         
+    let stdin = std::io::stdin();
+    let stdin = stdin.lock();
+
+    let deserializer = serde_json::Deserializer::from_reader(stdin);
+    let iterator = deserializer.into_iter::<serde_json::Value>();
+    for item in iterator {
+        println!("Got {:?}", item?);
+    }
+
+    Ok(())
+
 }

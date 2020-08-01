3301127257904da64f92d5cd2867d14312724935
Aman Berry <aman.berry725@gmai.com>
2020-08-01 18:43:16 +0100

    start on main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+extern crate serde_derive;
+
+use serde_json::{Result, Value};
+use std::fs::File;
+use std::io::copy;
+use std::io::stdout;
+
+fn main() -> Result<()> {
+         
 }

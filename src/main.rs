5b3c9c31eaaad47420f3e97f6ee443db02d54901
am-berry <aman.berry725@gmail.com>
2020-08-02 03:03:59 +0100

    work on trawling files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 extern crate ajson;
 extern crate csv;
 
+use std::path::Path;
 use std::error::Error;
-use std::fs::File;
+use std::fs::{File, read_dir};
 use std::io::{BufRead, BufReader};
 use csv::Writer;
+use std::io;
+
+fn trawl_files() -> Result<Vec<String>, Box<dyn Error>> {
+    // want to look through a directory, and get a list of 
+    // .json files  
+    let mut files = read_dir("./data/")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+    Ok(files)
+}
+
 
 fn parse_json(p: String) -> Result<Vec<String>, Box<dyn Error>> {
     let mut vals = Vec::new();
@@ -30,6 +42,8 @@ fn csv_conv(V: Vec<String>) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
+    let mut files = trawl_files().unwrap();
+    println!("{:?}", files)
     let mut vals = parse_json("./src/data/2011-01.json".to_string());
     let vals = vals.unwrap();
     println!("{}", vals.len());

cf2f16b4bc9516419efdc39020a5a097f09891ba
am-berry <aman.berry725@gmail.com>
2020-08-02 14:25:42 +0100

    work

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::io::{BufRead, BufReader};
 use csv::Writer;
 use std::io;
 
+/*
 fn trawl_files() -> Result<Vec<String>, Box<dyn Error>> {
     // want to look through a directory, and get a list of 
     // .json files  
@@ -16,19 +17,23 @@ fn trawl_files() -> Result<Vec<String>, Box<dyn Error>> {
         .collect::<Result<Vec<_>, io::Error>>()?;
     Ok(files)
 }
+*/
 
+//write a regex func which finds the summary through end of line
 
 fn parse_json(p: String) -> Result<Vec<String>, Box<dyn Error>> {
     let mut vals = Vec::new();
     let f = File::open(p.to_string()).unwrap();
     let reader = BufReader::new(f);
-
+    let mut count = 0;
     for item in reader.lines() {
         let txt = ajson::get(&item.unwrap(), "selftext");
         let txt = txt.unwrap();
         let lower = txt.as_str().to_lowercase();
         if lower.contains("tl;dr") | lower.contains("tl:dr") {
             vals.push(lower);
+            println!("------------------------------------------------------------------");
+            println!("{}", txt);
         }
     }
     Ok(vals)
@@ -42,8 +47,8 @@ fn csv_conv(V: Vec<String>) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let mut files = trawl_files().unwrap();
-    println!("{:?}", files)
+    //let mut files = trawl_files().unwrap();
+    //println!("{:?}", files);
     let mut vals = parse_json("./src/data/2011-01.json".to_string());
     let vals = vals.unwrap();
     println!("{}", vals.len());

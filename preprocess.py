fd420b7b574996b5c1bbcab4a3465a10d3b10672
am-berry <aman.berry725@gmail.com>
2020-11-23 16:09:04 +0000

    add preprocess code for LEAD-N

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import nltk
 
+'''
 d = pd.read_csv('res.csv') 
 
 def f(x):
@@ -29,3 +31,15 @@ d.drop_duplicates(inplace=True)
 d.drop('Summary', axis= 1,inplace=True)
 
 d.to_csv('processed.csv', sep = ',', index=False)
+'''
+nltk.download('punkt')
+
+d = pd.read_csv('processed.csv', sep=',', error_bad_lines=False)
+print(d['Sum'].isna().sum())
+d.dropna(inplace=True)
+print(d['Sum'].isna().sum())
+d['sum_sents'] = d['Sum'].apply(nltk.sent_tokenize)
+d['sum_len'] = d['sum_sents'].apply(len)
+
+print(d.head(10))
+d.to_csv('prp.csv', sep=',', index=False)

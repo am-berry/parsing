f8ba2639bff03b8de82cdd93ea3888a7486813c6
am-berry <aman.berry725@gmail.com>
2020-12-11 19:49:07 +0000

    updates

--- baseline/random_n.py
+++ baseline/random_n.py
@@ -1,15 +1,28 @@
 import random
+<<<<<<< HEAD
+import string
+
+=======
+>>>>>>> 39595f8a87b536d94828777e7220a7fa8dde1ecb
 import numpy as np
 import pandas as pd
 import nltk
 import rouge
 
-nltk.download('punkt')
-
 df = pd.read_csv('../data/test.csv')
 
 df['tokenized_text'] = df['Text'].apply(nltk.sent_tokenize)
 
+rands = df['tokenized_text'].apply(lambda x: random.sample(x,1))
+gens = rands.apply(lambda x: ''.join([str(a) for a in x])).tolist()
+refs = df['Sum'].tolist()
+print(len(gens), len(refs))
+
+gen_ref = zip(gens, refs)
+gen_ref = [_ for _ in gen_ref if not all(j in string.punctuation for j in _[0])]
+gens, refs  = zip(*gen_ref)
+print(len(gens), len(refs))
+ 
 def get_n_random(x):
     return random.sample(x.tokenized_text, k=x.sum_len) 
 
--- lexrank/lexrank.py
+++ lexrank/lexrank.py
@@ -7,11 +7,21 @@ import numpy as np
 from lexrank_utils import *
 import time
 import pickle
+<<<<<<< HEAD
+import gc
+import string 
+
+t = time.time()
+oa_scores = []
+
+for a in [(30001, -1)]:
+=======
 
 t = time.time()
 scores = []
 
 for a in [(10,30000), (30001, -1)]:
+>>>>>>> 39595f8a87b536d94828777e7220a7fa8dde1ecb
     test = pd.read_csv('../data/test.csv', sep=',')[a[0]:a[1]]
     print(test[test['sum_len'] == 0].sum())
     model = SentenceTransformer('../models/', device='cuda')
@@ -73,15 +83,35 @@ for a in [(10,30000), (30001, -1)]:
     test['joined'] = test['generated'].apply(lambda x: ' '.join([str(a) for a in x]))
     test[['Text', 'Sum', 'joined']].to_csv('lexrank_results.csv', index=False, sep=',')
 
+<<<<<<< HEAD
+    gens = test['joined'].tolist()
+    refs = test['Sum'].tolist()
+
+    gen_ref = zip(gens, refs)
+    gen_ref = [_ for _ in gen_ref if not all(j in string.punctuation for j in _[0])]
+    gens, refs  = zip(*gen_ref)
+
+    from rouge import Rouge
+    rouge = Rouge()
+    score = rouge.get_scores(gens, refs, avg=True, ignore_empty=True)
+=======
     from rouge import Rouge
     rouge = Rouge()
     score = rouge.get_scores(test['joined'].tolist(), test['Sum'].tolist(), avg=True, ignore_empty=True)
+>>>>>>> 39595f8a87b536d94828777e7220a7fa8dde1ecb
     import json
     out_file = f'{a[0]}_lexrank.json'
     with open(out_file, 'w') as handler:
             json.dump(score, handler)
 
     print(score)
+<<<<<<< HEAD
+    oa_scores.append(score)
+    gc.collect()
+
+print(oa_scores)
+=======
     scores.append(score)
 
 print(scores)
+>>>>>>> 39595f8a87b536d94828777e7220a7fa8dde1ecb
